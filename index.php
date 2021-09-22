e1b82d9baa34923fa1c82af86e168f9fa59454c2
rubenvandeven2001 <484553@student.fontys.nl>
2021-09-22 13:04:26 +0200

    Application page indexation

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
+    // Include Twig-templating engine
     require_once 'vendor/autoload.php';
 
+    // Define the baseUrl
+    $baseUrl = "http://localhost/school-projects/fontys-portal-proto-v1/";
+    // Get the URL of the current requested page
+    $pageUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    // Store the page-specifier from the URL
+    $pageSpecifier = str_replace($baseUrl, "", $pageUrl);
+    
+    // Let twig read the templates folder
     $loader = new \Twig\Loader\FilesystemLoader('templates');
+    // Create a new Twig envirorment (workspace)
     $twig = new \Twig\Environment($loader);
-    echo $twig->render('index.twig', ['name' => 'Fabien']);
+    // Render the base twig-file
+    echo $twig->render('/layouts/base.twig', ['page' => $pageSpecifier]);
 ?>
