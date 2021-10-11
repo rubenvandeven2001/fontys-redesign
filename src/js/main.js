094c3638e85ce29f05d7648186e121ee4445479c
rubenvandeven2001 <484553@student.fontys.nl>
2021-10-11 12:38:11 +0200

    Added a favicon

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,14 @@
-var Chart = require('chart.js');
+import "./pages/lesson-material";
 
-const dates = ["20 september", "21 september", "22 september"];
-const feedback = [0, 1, 1];
+let navigationOpen = false;
+const navigation = document.getElementsByClassName("navigation")[0];;
 
-generateChart(dates, feedback);
+document.getElementsByClassName("navigation__close")[0].addEventListener("click", navigationToggle);
+document.getElementsByClassName("header__hamburger-button")[0].addEventListener("click", navigationToggle);
 
-function generateChart(chartDate, chartFeedback) {
-	var chart = new Chart(document.getElementById('feedpulse__canvas').getContext('2d'), {
-        type: 'line',
-        data: {
-            labels: chartDate,
-                datasets: [{
-                    type: "line",
-                        label: "Feedpulse feedback",
-                        borderColor: '#fd4c4c',
-                        data: chartFeedback,
-                }]
-        },
-        options: {
-            scales: {
-                xAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Datums'
-                    },
-                }],
-                yAxes: [{
-                    display: true,
-                    ticks: {
-                        stepSize: 1,
-                        suggestedMin: -1,
-                        suggestedMax: 1
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Feedback'
-                    }
-                }]
-            },
-            elements: { point: { radius: 5 } }
-        }
-	});
+function navigationToggle (event) {
+    event.preventDefault();
+    if (navigationOpen) navigation.style.transform = "translateX(-100%)";
+    else navigation.style.transform = "translateX(0%)";
+    navigationOpen = !navigationOpen;
 }
